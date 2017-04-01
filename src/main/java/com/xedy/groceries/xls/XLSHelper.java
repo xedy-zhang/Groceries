3973bcb4bce3252539e2177a0ceda2be3c51825b
xedy.zhang <xedy_zhang@126.com>
2017-04-01 16:33:31 +0800

    提交编程杂项

---
+++ src/main/java/com/xedy/groceries/xls/XLSHelper.java
@@ -0,0 +1,123 @@
+/**
+ * 
+ */
+package com.xedy.groceries.xls;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import jxl.Workbook;
+import jxl.write.Label;
+import jxl.write.WritableSheet;
+import jxl.write.WritableWorkbook;
+import jxl.write.WriteException;
+import jxl.write.biff.RowsExceededException;
+
+/**
+ * xls文件处理工具类。使用两种处理技术：<i>jxl</i>、<i>poi</i>。
+ * 
+ * @author xedy_zhang@126.com
+ *
+ */
+public class XLSHelper {
+	
+	/**
+	 * 将数据写入xls文件。如果<b><i>datas</i></b>超过<b>65536行256列</b>，则仅写入<b>65536行256列</b>条数据。超过<b>65536行256列</b>请使用{@link #writeXlsx}
+	 * @param datas 待写入xls数据集。
+	 * @param filePath 文件路径。文件不存在时将创建。
+	 * @throws IOException 文件创建，写入异常
+	 */
+	public static void writeXls(List<List<String>> datas,String filePath) throws IOException{
+		if(datas != null && datas.size() > 0){
+			return;
+		}
+		File file = fileCreator(filePath);
+		writeXlsWithJxl(file,datas);
+	}
+	
+	/**
+	 * 根据指定文件完整路径创建文件
+	 * @param filePath 文件完整路径
+	 * @return 已创建的文件
+	 * @throws IOException 文件创建异常
+	 */
+	private static File fileCreator(String filePath) throws IOException{
+		File file  = new File(filePath);
+		if(!(file.isFile() && file.exists())){
+			file.createNewFile();
+		}
+		return file;
+	}
+	
+	/**
+	 * 将数据写入xls文件。获取过程嵌入到xls创建过程。
+	 * @param filePath 文件路径。文件不存在时将创建。
+	 * @param processor 单元格处理器
+	 * @throws IOException 文件创建异常
+	 */
+	public static void writeXls(String filePath,AddSheetCellProcessor processor) throws IOException{
+		File file = fileCreator(filePath);
+		writeXlsWithJxl(file,processor);
+	}
+	
+	/**
+	 * 将数据获取嵌入到xls创建过程
+	 * @param file 已创建xls文件
+	 * @param processor 外部扩展处理实现
+	 * @throws IOException 数据写入异常
+	 */
+	private static void writeXlsWithJxl(File file , AddSheetCellProcessor processor) throws IOException{
+		WritableWorkbook wb = Workbook.createWorkbook(file);
+		WritableSheet sheet = wb.createSheet("SHEET 1", 0);
+		sheet.getSettings().setVerticalFreeze(1);
+		try {
+			processor.doProcessor(sheet);
+			wb.write();
+			wb.close();
+		} catch (WriteException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * 使用jxl工具创建xls文件
+	 * @param file xls文件对象
+	 * @param datas 待写入xls文件数据集合
+	 * @throws IOException 数据写入异常
+	 */
+	private static void writeXlsWithJxl(File file,List<List<String>> datas) throws IOException{
+		WritableWorkbook wb = Workbook.createWorkbook(file);
+		WritableSheet sheet = wb.createSheet("SHEET 1", 0);
+		sheet.getSettings().setVerticalFreeze(1);
+		try {
+			for (int i = 0; (i < datas.size()) && i < 65536; i++) {
+				List<String> data = datas.get(i);
+				for (int x = 0 ; (x < data.size() && x < 256) ; x++) {
+					sheet.addCell(new Label(x, i, data.get(x)));
+				}
+			}
+			wb.write();
+			wb.close();
+		} catch (WriteException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * 将数据写入xlsx文件。如果<b><i>datas</i></b>超过<b>1048576行16384列</b>，则仅写入<b>1048576行16384列</b>条数据。
+	 * @param datas 待写入xls数据集。
+	 * @param filePath 文件路径。文件不存在时将创建。
+	 */
+	public static void writeXlsx(List<List<String>> datas,String filePath){
+		//TODO: 待实现POI技术创建XLSX文件
+	}
+}
+/**
+ * 增加sheet单元格
+ * @author xedy_zhang@126.com
+ *
+ */
+interface AddSheetCellProcessor{
+	public void doProcessor(WritableSheet sheet) throws RowsExceededException, WriteException;
+}
---
+++ src/test/java/com/xedy/groceries/xls/XLSHelperTest.java
@@ -0,0 +1,57 @@
+package com.xedy.groceries.xls;
+
+import java.io.IOException;
+import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+import junit.framework.TestCase;
+import jxl.write.Label;
+import jxl.write.WritableSheet;
+import jxl.write.WriteException;
+import jxl.write.biff.RowsExceededException;
+
+public class XLSHelperTest extends TestCase {
+	private String filePath="test.xls";
+	private List<List<String>> datas = new ArrayList<List<String>>();
+	
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+		for(int i = 0 ; i < 1000 ; i++){
+			List<String> data = new ArrayList<String>();
+			for(int x = 0 ; x < 10 ; x++){
+				if(i==0){
+					data.add("标题行"+x);
+				}else{
+					data.add(MessageFormat.format("row{0}-colum{1}", new String[]{String.valueOf(i),String.valueOf(x)}));
+				}
+			}
+			datas.add(data);
+		}
+	}
+	
+	public void testWriteXls() {
+		
+		try {
+			XLSHelper.writeXls(datas, filePath);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		assertEquals(true, true);
+	}
+	
+	public void testWriteXls_DataEmbedded(){
+		
+		try {
+			XLSHelper.writeXls("xls-embedded.xls",new AddSheetCellProcessor(){
+				public void doProcessor(WritableSheet sheet) throws RowsExceededException, WriteException {
+					sheet.addCell(new Label(0, 0, "嵌入xls创建过程..."));
+				}
+			});
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		assertEquals(true, true);
+	}
+}
